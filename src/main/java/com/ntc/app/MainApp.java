eb9fc85523708e8068d8ae9814c2dd810b3b6799
nghiatc <congnghia0609@gmail.com>
2020-01-06 13:39:17 +0700

    update code

--- src/main/java/com/ntc/app/MainApp.java
+++ src/main/java/com/ntc/app/MainApp.java
@@ -41,17 +41,48 @@ public class MainApp {
             // test3
             //test3();
             
+            // test7
+            test7();
+            
             // test6
-            test6();
+            //test6();
             
             // test4
             //test4();
             
             //test5
             //test5();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void test7() {
+        try {
+            String s = "nghiatcxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+            SSS sss = new SSS();
+            List<String> arr = sss.create2(3, 6, s);
+            //System.out.println(arr);
+            for (int i=0; i<arr.size(); i++) {
+                System.out.println("shares["+i+"]: " + arr.get(i));
+            }
             
+            System.out.println("secret: " + s);
+            System.out.println("secret.length: " + s.length());
+            String s1 = sss.combine2(arr.subList(0, 3));
+            System.out.println("combines shares 1 length = " + arr.subList(0, 3).size());
+            System.out.println("secret: " + s1);
+            System.out.println("secret.length: " + s1.length());
             
+            String s2 = sss.combine2(arr.subList(3, 6));
+            System.out.println("combines shares 2 length = " + arr.subList(3, 6).size());
+            System.out.println("secret: " + s2);
+            System.out.println("secret.length: " + s2.length());
             
+            String s3 = sss.combine2(arr.subList(1, 5));
+            System.out.println("combines shares 3 length = " + arr.subList(1, 5).size());
+            System.out.println("secret: " + s3);
+            System.out.println("secret.length: " + s3.length());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,14 +93,29 @@ public class MainApp {
             String s = "nghiatcxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
             SSS sss = new SSS();
             List<String> arr = new ArrayList<>();
-            arr.add("RcMcipRzXitM2lN3ODj0ozPan6iJryLlaxBTNWIitqU=Jj6lPtzHalvXX_JEJmWoddAxiTxpOGu0mCUgqM0tCaY=bC7cF5u1-bVP02_1N92Dt6QMOXaI7YQcyBwcYhzU4EE=ltsqqJAwqqMQ3HQwFkw28O44fKC2cv9p_xYLsdO_Yd8=2DLzR4WObHgH2JStKjvGrPVHeWWyv94Qo5RPoTLSp0c=OMjTafnolX3x4OWWqbchNrjOuTAfVte84LzAqGZMdic=xjPulHqtcFP8WNGQsdaLmxxaKd0NSOX2U9OQsYXEJ-0=AhFNmL1VvJbmysNrEX3GmSZLqKtjPdxQejCSWgvd4ro=");
-            arr.add("UgXXq58iTlpxlogh0Ravvpl0qSC4-NFwdENYgKO3JSU=H6gLwZvdNVaI5MVgNI3kQY1UCt3boOrQDmHYoLjsTAw=g985sDTPhZZvEqrKoCfCcBHYceR9kRSJIRktujbPdIk=L6TiRDODiNDz164nPcb9naxAqbxB6xK4J91tU9gG4oE=7QadnJFqYFSd19ZflXC3bseOdWquGK7hFpPG3nBKeYg=IcTLqH6ke4Oj5MOif0jlOe9rG-NXIkDG6qSAdOArRsg=deZknY1xgIweueUVPVOVyjRlxyOgtZ78H1gyvXR5zAY=c9UXuLQxcEcwwwFX-lXHOUyE-2tRp1dqWzk570Ko1mo=");
-            arr.add("eingJCBI0MzKmE0wbKHg2kuPMWPduywHywB5bS6rByc=oi3l6TNphyTXXxiGMsl8Owg3EtKt_9c_JwtTzzofwg0=u1n9Nn2liUkVCxhxfNGkwRj10ZnP_j0I6uk7cdiztkQ=DpQylMT8QAQsOvrlK16Abgim8aAG-Hch_5bmCvc4MoQ=bwX6Kltuv3jY-Zg3SeW5IYu--m2LrsYHdPQywhidxa4=qxoFLhfm5s6fSInY_Nvn-OknGXFfGsT4e7BNTEWZNlE=ZBcGI1Ly7LdKz23J4N04VvSC7loKspIHdiOIeOF2Lf4=tvh88JTgtZyJoo8429NmKIrPTuvJYZPj1Nv10qOTBZk=");
+            arr.add("-6kRAYzbOJ7Bzix7Ho2MllDFsPWoRn5jJdtjN-Jh6u0=WNAC1C43DgizLFxx9tztNtajaRipLorHCjyVEK7IbCE=rs6Ldl2b1BHMV-g6tostDCGsbzgAD6MRI9E4JHdS1zY=iuX9lmVpMnFmvUihuIZnaA0wo0QgqxJcS61Pd8tDbOY=AgHOXOZ51Ir8DzIcLAGIastwVeYeU46N2yb3F-OVlUM=Sh7Od5Nj4D8mT8ICLUNpJUkNAQnImWkcQGqzobGgN98=ztDxUHU5lqumJhe2PaGuwWJqlcMUAUGEzeqwf9Gi0FE=PJ9HU92KhgSb1gk7110DZWaD0zEinuTzrtZ75Tpdy7g=");
+            arr.add("PGnifsn0E47aTPjGROtUIdCeybbeVis7BdJXcr7bhvE=AOX8JqnX6SU61MFUscFYtMXH4u6vXqhs3xscH_mZHxs=iCkv-b1UEtdHHViAcFWXAIK77S3_J4AmPx0YurzrJWI=pGX2-_9iw89cJl94NT1_7xWdmjRLQFm69sG2rnjMQyI=Cr2L8oCqIi9EKR6BCT6c9yx-rmQeCLqdZS71uUfVErI=qQjqe8PR_k4sokN45SozVvvp3xkcKe2h67B7eMbWekQ=zMaw6I4rXjjnd18dCbI_ErD-drdihERmFtwTivu-MpM=WPewg1Il5CzD0T9qQDU9e3473Fe4n5hcXMt9ZgtIPMw=");
+            arr.add("tjt0-OscuPvII8MdurlJ7IKt518Dh5CB3Ja6hIUHHG8=g8mf7n0-Yo_AKMRJQnzyWAhlutYMW7xn9ZpoCxFMMts=IGUn5CTH2B0m5LoOkfqpeurC3nikWGHFNOJ3zQoIhms=gQmy8mWlUA6nCy4sP_EhsquFJ_yM0OV3RXW0H6Aki4I=1_m0WCeVZnyGbYi3S-O9Fk8NoxaGIMLU5mYIH1Ho15o=-m8F5ObRHSRd5grwK8ibBE41nKm35eD78bQr_783FVs=MX1GtfXetB9BbvkfeOW7vyyYJAtc6PCFIUsVAElRbDo=NXJemTh4-F9ft64F9jIeLdH5ap8PiqXOOC9RyJf5hzY=");
+            arr.add("9KYl1DQKRqkugNhsbmsTr2oGMX4nwMMmkbdolduW52U=fPN17lGFPRNGCXDAmNBCepJED7VdiqznFYagIWK8-KA=X2lgZwYoCsmDcU-AjskA5_ybtmKaLMRj9zmQOm1c8jY=2DsLN-EteKyEUAYQ14qIHRYemEeGAs9Sl9PDLOgzRvY=tyU8vyiIm1WoLe5U2nDQaU2mZlM-cKKeEdZFYz3w4Hs=uoiH1q8rfzqj76Q-WU4rI4-45qjWfg7CLoTWj_HJVGQ=YkjdzOpgxyEpXb20M7ZAwfJarywA2jU42dADDiM4arg=FCBA4qL0rEBbuJMqVNaftWqEEt1s95ZqMLJmEmWNCSQ=");
+            arr.add("5SfGYvum00lZyxnKC6F7eu9k8_1vplEe1FKJKlJmwgM=BQuq8pbN4sHyevmpOzW4x-44wvXF0F0EoeI2EoD3naE=K9RtoD7O8hT1Kg3JjoEN7QyhFwgxq1bfm4tBYQEQVaM=wX9HOEqXXEB4bzIfLTqkG2CGqGe0zJkCqnpFCnQBlpI=5lsrk9yqlYvPXN4qWABpte5NYcZuUKE7j26-iDIPdXs=3NRI3z2lXmPpP-lJ9IimWPHJ5SxaIZvCgCBLbtWNzoY=qW8apzgoff0FfN-nsQ1qoX6D15fxXsRVOYDouA79rV4=S5rSKUHeDSH4jxzp7Wtuhbf2-M8PCIHmealP9V43Qhc=");
+            arr.add("RNAt2VUwkL4Ip22-pdhoWEP_rUv8N2CSZvAu1_X7Lgo=m7CONLOElJUz1liZ3gNakUw8kgnOl5gXpoIjgNsztz0=KYSIRF6X75jHi36-TLox7g9Cug0akg_QRxMg6lyPtQA=6UOeGwZZIEH1DwGfd2S8ETzRDPLT19imSylh48JK9V8=P9qjc81sVLyxI-fyKpq-vmhcvcSCoLnN7Yh64yFK9SA=26bJwPGErnws19RRjk-m5At3z8QY0VRdZCFOOhD8mMg=FYAI4YJ8r0z7FH59d0p0y7WVxlVOdhWCK2UqnImvNhw=20ozw45I4Wlgnq_8J9RbGXlWU7FHCn7MCdJbezxk-TE=");
             
             System.out.println("secret: " + s);
-            String s1 = sss.combine(arr);
-            System.out.println("combines shares 1 length = " + arr.size());
+            System.out.println("secret.length: " + s.length());
+            String s1 = sss.combine(arr.subList(0, 3));
+            System.out.println("combines shares 1 length = " + arr.subList(0, 3).size());
             System.out.println("secret: " + s1);
+            System.out.println("secret.length: " + s1.length());
+            
+            String s2 = sss.combine(arr.subList(3, 6));
+            System.out.println("combines shares 2 length = " + arr.subList(3, 6).size());
+            System.out.println("secret: " + s2);
+            System.out.println("secret.length: " + s2.length());
+            
+            String s3 = sss.combine(arr.subList(1, 5));
+            System.out.println("combines shares 3 length = " + arr.subList(1, 5).size());
+            System.out.println("secret: " + s3);
+            System.out.println("secret.length: " + s3.length());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -86,13 +132,16 @@ public class MainApp {
             }
             
             System.out.println("secret: " + s);
+            System.out.println("secret.length: " + s.length());
             String s1 = sss.combine(arr.subList(0, 3));
             System.out.println("combines shares 1 length = " + arr.subList(0, 3).size());
             System.out.println("secret: " + s1);
+            System.out.println("secret.length: " + s1.length());
             
             String s2 = sss.combine(arr.subList(1, 5));
             System.out.println("combines shares 2 length = " + arr.subList(1, 5).size());
             System.out.println("secret: " + s2);
+            System.out.println("secret.length: " + s2.length());
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/ntc/jcrypto/sss/SSS.java
+++ src/main/java/com/ntc/jcrypto/sss/SSS.java
@@ -49,8 +49,8 @@ public class SSS {
         
         // Convert the secret to its respective 256-bit big.Int representation
         List<BigInteger> secrets = splitSecretToBigInt(secret);
-        System.out.println("====================== create ======================");
-        System.out.println(secrets);
+        //System.out.println("====================== create ======================");
+        //System.out.println(secrets);
         
         // List of currently used numbers in the polynomial
         List<BigInteger> numbers = new ArrayList<>();
@@ -111,6 +111,88 @@ public class SSS {
                 s += toBase64(points[i][j][0]);
                 //System.out.println("x[share-"+i+"][part-"+j+"]: " + points[i][j][0].toString(10));
                 s += toBase64(points[i][j][1]);
+                //System.out.println("y[share-"+i+"][part-"+j+"]: " + points[i][j][1].toString(10));
+            }
+            rs.add(s);
+        }
+        //System.out.println("====================== create ======================");
+        //System.out.println(Arrays.deepToString(points));
+        return rs;
+    }
+    
+    public List<String> create2(int minimum, int shares, String secret) {
+        // Verify minimum isn't greater than shares; there is no way to recreate
+        // the original polynomial in our current setup, therefore it doesn't make
+        // sense to generate fewer shares than are needed to reconstruct the secret.
+        List<String> rs = new ArrayList<>();
+        if (minimum > shares) {
+            throw new ExceptionInInitializerError("cannot require more shares then existing");
+        }
+        
+        // Convert the secret to its respective 256-bit big.Int representation
+        List<BigInteger> secrets = splitSecretToBigInt(secret);
+        //System.out.println("====================== create ======================");
+        //System.out.println(secrets);
+        
+        // List of currently used numbers in the polynomial
+        List<BigInteger> numbers = new ArrayList<>();
+        numbers.add(BigInteger.ZERO);
+        
+        // Create the polynomial of degree (minimum - 1); that is, the highest
+        // order term is (minimum-1), though as there is a constant term with
+        // order 0, there are (minimum) number of coefficients.
+        //
+        // However, the polynomial object is a 2d array, because we are constructing
+        // a different polynomial for each part of the secret
+        // polynomial[parts][minimum]
+        BigInteger[][] polynomial = new BigInteger[secrets.size()][minimum];
+        for (int i=0; i<secrets.size(); i++) {
+            polynomial[i][0] = secrets.get(i);
+            for (int j=1; j<minimum; j++) {
+                // Each coefficient should be unique
+                BigInteger number = random();
+                while (inNumbers(numbers, number)) {
+                    number = random();
+                }
+                numbers.add(number);
+                
+                polynomial[i][j] = number;
+            }
+        }
+        //System.out.println("====================== polynomial ======================");
+        //System.out.println(Arrays.deepToString(polynomial));
+        
+        // Create the secrets object; this holds the (x, y) points of each share.
+        // Again, because secret is an array, each share could have multiple parts
+        // over which we are computing Shamir's Algorithm. The last dimension is
+        // always two, as it is storing an x, y pair of points.
+        //
+        // Note: this array is technically unnecessary due to creating result
+        // in the inner loop. Can disappear later if desired. [TODO]
+        //
+        // points[shares][parts][2]
+        BigInteger[][][] points = new BigInteger[shares][secrets.size()][2];
+        
+        // For every share...
+        for (int i=0; i<shares; i++) {
+            String s = "";
+            // ...and every part of the secret...
+            for (int j=0; j<secrets.size(); j++) {
+                // ...generate a new x-coordinate...
+                BigInteger number = random();
+                while (inNumbers(numbers, number)) {
+                    number = random();
+                }
+                numbers.add(number);
+                
+                // ...and evaluate the polynomial at that point...
+                points[i][j][0] = number;
+                points[i][j][1] = evaluatePolynomial(polynomial, j, number);
+                
+                // ...add it to results...
+                s += toHex(points[i][j][0]);
+                //System.out.println("x[share-"+i+"][part-"+j+"]: " + points[i][j][0].toString(10));
+                s += toHex(points[i][j][1]);
                 //System.out.println("y[share-"+i+"][part-"+j+"]: " + points[i][j][1].toString(10));
             }
             rs.add(s);
@@ -202,8 +284,90 @@ public class SSS {
         }
         
         // ...and return the result!
-        System.out.println("====================== combine ======================");
-        System.out.println(secrets);
+        //System.out.println("====================== combine ======================");
+        //System.out.println(secrets);
+        rs = mergeBigIntToString(secrets);
+        return rs;
+    }
+    
+    public String combine2(List<String> shares) throws Exception {
+        String rs = "";
+        if (shares == null || shares.isEmpty()) {
+            throw new Exception("shares is NULL or empty");
+        }
+        
+        // Recreate the original object of x, y points, based upon number of shares
+        // and size of each share (number of parts in the secret).
+        // points[shares][parts][2]
+        BigInteger[][][] points = new BigInteger[shares.size()][][];
+        
+        // For each share...
+        for (int i=0; i<shares.size(); i++) {
+            // ...ensure that it is valid...
+            if (isValidShareHex(shares.get(i)) == false) {
+                throw new Exception("one of the shares is invalid");
+            }
+            
+            // ...find the number of parts it represents...
+            String share = shares.get(i);
+            int count = share.length() / 128;
+            //System.out.println("count: " + count);
+            points[i] = new BigInteger[count][];
+            
+            // ...and for each part, find the x,y pair...
+            for (int j=0; j<count; j++) {
+                points[i][j] = new BigInteger[2];
+                String cshare = share.substring(j*128, (j+1)*128);
+                // ...decoding from base 64.
+                points[i][j][0] = fromHex(cshare.substring(0, 64));
+                points[i][j][1] = fromHex(cshare.substring(64, 128));
+            }
+        }
+        //System.out.println("====================== combine ======================");
+        //System.out.println(Arrays.deepToString(points));
+        
+        // Use Lagrange Polynomial Interpolation (LPI) to reconstruct the secret.
+        // For each part of the secret (clearest to iterate over)...
+        List<BigInteger> secrets = new ArrayList<>();
+        int numSecret = points[0].length;
+        //System.out.println("numSecret: " + numSecret);
+        for (int j=0; j<numSecret; j++) {
+            secrets.add(BigInteger.ZERO);
+            // ...and every share...
+            for (int i=0; i<shares.size(); i++) { // LPI sum loop
+                //System.out.println("i: " + i);
+                // ...remember the current x and y values...
+                BigInteger origin = points[i][j][0];
+                BigInteger originy = points[i][j][1];
+                BigInteger numerator = BigInteger.ONE; // LPI numerator
+                BigInteger denominator = BigInteger.ONE; // LPI denominator
+                // ...and for every other point...
+                for (int k=0; k<shares.size(); k++) { // LPI product loop
+                    if (k != i) {
+                        // ...combine them via half products...
+                        BigInteger current = points[k][j][0];
+                        BigInteger negative = current.multiply(new BigInteger("-1")).mod(PRIME);
+                        BigInteger added = origin.subtract(current);
+                        numerator = numerator.multiply(negative).mod(PRIME);
+                        denominator = denominator.multiply(added).mod(PRIME);
+                    }
+                }
+                
+                // LPI product
+                // ...multiply together the points (y)(numerator)(denominator)^-1...
+                BigInteger working = originy.multiply(numerator).mod(PRIME);
+                working = working.multiply(denominator.modInverse(PRIME)).mod(PRIME);
+                
+                // LPI sum
+                BigInteger secret = secrets.get(j);
+                secret = secret.add(working).mod(PRIME);
+                secrets.set(j, secret);
+            }
+        }
+        
+        // ...and return the result!
+        //System.out.println("====================== combine ======================");
+        //System.out.println(secrets);
         rs = mergeBigIntToString(secrets);
         return rs;
     }
@@ -248,6 +412,33 @@ public class SSS {
         return rs;
     }
     
+//    public List<BigInteger> splitSecretToBigInt2(String secret) {
+//        List<BigInteger> rs = new ArrayList<>();
+//        if (secret != null && !secret.isEmpty()) {
+//            byte[] sbyte = secret.getBytes(StandardCharsets.UTF_8);
+//            String hexData = encodeHexString(sbyte);
+//            //System.out.println("hexData: " + hexData);
+//            int count = (int) Math.ceil(hexData.length() / 64.0);
+//            //System.out.println("secret part count: " + count);
+//            for (int i=0; i<count; i++) {
+//                if ((i+1)*64 < hexData.length()) {
+//                    BigInteger bi = new BigInteger(hexData.substring(i*64, (i+1)*64), 16);
+//                    rs.add(bi);
+//                } else {
+//                    String last = hexData.substring(i*64, hexData.length());
+//                    String pading = "";
+//                    for (int j=0; j<(64-last.length()); j++) {
+//                        pading += "0";
+//                    }
+//                    last = pading + last;
+//                    BigInteger bi = new BigInteger(last, 16);
+//                    rs.add(bi);
+//                }
+//            }
+//        }
+//        return rs;
+//    }
+    
     // Converts an array of big.Ints to the original byte array, removing any
     // least significant nulls
     public String mergeBigIntToString(List<BigInteger> secrets) {
@@ -264,9 +455,28 @@ public class SSS {
         }
         byte[] byteData = decodeHexString(hexData);
         byteData = trimRight(byteData);
+        //System.out.println("byteData: " + Arrays.toString(byteData));
         rs = new String(byteData, StandardCharsets.UTF_8);
         return rs;
     }
+    
+//    public String mergeBigIntToString2(List<BigInteger> secrets) {
+//        String rs = "";
+//        String hexData = "";
+//        for (BigInteger s : secrets) {
+//            String tmp = s.toString(16);
+//            //System.out.println("tmp: " + tmp);
+//            String pading = "";
+//            for (int j=0; j<(64-tmp.length()); j++) {
+//                pading += "0";
+//            }
+//            hexData = hexData + pading + tmp;
+//        }
+//        byte[] byteData = decodeHexString(hexData);
+//        byteData = trimRight(byteData);
+//        rs = new String(byteData, StandardCharsets.UTF_8);
+//        return rs;
+//    }
     
     // https://stackoverflow.com/questions/17003164/byte-array-with-padding-of-null-bytes-at-the-end-how-to-efficiently-copy-to-sma
     public byte[] trimRight(byte[] bytes) {
@@ -314,8 +524,25 @@ public class SSS {
     // Returns the big.Int number base10 in base64 representation; note: this is
     // not a string representation; the base64 output is exactly 256 bits long
     public String toBase64(BigInteger number) {
+        String hexdata = number.toString(16);
+        int n = 64 - hexdata.length();
+        for (int i=0; i<n; i++) {
+            hexdata = "0" + hexdata;
+        }
+        //System.out.println("hexdata: " + hexdata);
+        return Base64.getUrlEncoder().encodeToString(decodeHexString(hexdata));
         //return Base64.getUrlEncoder().encodeToString(number.toByteArray());
-        return Base64.getEncoder().encodeToString(number.toByteArray());
+        //return Base64.getEncoder().encodeToString(number.toByteArray());
+    }
+    
+    public String toHex(BigInteger number) {
+        String hexdata = number.toString(16);
+        int n = 64 - hexdata.length();
+        for (int i=0; i<n; i++) {
+            hexdata = "0" + hexdata;
+        }
+        //System.out.println("hexdata: " + hexdata);
+        return hexdata;
     }
     
     // Returns the number base64 in base 10 big.Int representation; note: this is
@@ -323,8 +550,14 @@ public class SSS {
     // bits long, and the output is an arbitrary size base 10 integer.
     // Returns -1 on failure
     public BigInteger fromBase64(String number) {
-        //return new BigInteger(Base64.getUrlDecoder().decode(number));
-        return new BigInteger(Base64.getDecoder().decode(number));
+        byte[] bytedata = Base64.getUrlDecoder().decode(number);
+        String hexdata = encodeHexString(bytedata);
+        return new BigInteger(hexdata, 16);
+        //return new BigInteger(Base64.getDecoder().decode(number));
+    }
+    
+    public BigInteger fromHex(String number) {
+        return new BigInteger(number, 16);
     }
     
     // Takes in a given string to check if it is a valid secret
@@ -343,6 +576,25 @@ public class SSS {
         for (int i=0; i<count; i++) {
             String part = candidate.substring(i*44, (i+1)*44);
             BigInteger decode = fromBase64(part);
+            // decode < 0 || decode > PRIME ==> false
+            if (decode.compareTo(BigInteger.ONE) == -1 || decode.compareTo(PRIME) == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean isValidShareHex(String candidate) throws Exception {
+        if (candidate == null || candidate.isEmpty()) {
+            throw new Exception("String is NULL or empty.");
+        }
+        if (candidate.length()%128 != 0) {
+            return false;
+        }
+        int count = candidate.length() / 64;
+        for (int i=0; i<count; i++) {
+            String part = candidate.substring(i*64, (i+1)*64);
+            BigInteger decode = fromHex(part);
             // decode < 0 || decode > PRIME ==> false
             if (decode.compareTo(BigInteger.ONE) == -1 || decode.compareTo(PRIME) == 1) {
                 return false;
